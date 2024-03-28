bb94f4463a8d5ed4fb258f3cc56b5290d09b5348
otter502 <115671877+otter502@users.noreply.github.com>
2024-03-28 14:30:22 -0700

    added canBaseLogger tester

--- src/main/java/frc/robot/subsystems/tempSubsystem.java
+++ src/main/java/frc/robot/subsystems/tempSubsystem.java
@@ -1,5 +1,29 @@
 package frc.robot.subsystems;
 
-public class tempSubsystem {
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.CANBaseLogger;
+
+public class tempSubsystem extends SubsystemBase{
+    CANSparkMax mMotor;
+    GenericEntry io;
+
+    public tempSubsystem() {
+        super();
+        mMotor = new CANSparkMax(1, MotorType.kBrushless);
+        mMotor.setIdleMode(IdleMode.kCoast);
+        new CANBaseLogger(mMotor, "TestingLogger", "mainMotor");
+        var tab = Shuffleboard.getTab("testing");
+        io = tab.add("setMotor", 0.0).getEntry();
+    }
     
+    @Override
+    public void periodic() {
+        mMotor.set(io.getDouble(0.0));
+    }
 }
